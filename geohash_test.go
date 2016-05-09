912d583ce80837f2f2ce7d7da4a1d6220d16ad10
wangdekun <1151714332@qq.com>
2016-05-09 17:14:48 +0800

    geohash test

--- geohash_test.go
+++ geohash_test.go
@@ -13,7 +13,7 @@ func TestGeoHash(t *testing.T) {
 	fmt.Println(GetNeighbors(39.92324, 116.3906, 5))
 
 	// output:
-	// 	wx4g0
+	// wx4g0
 	// &{39.90234375 39.9462890625 116.3671875 116.4111328125}
 	// [wx4g0 wx4g2 wx4fb wx4ep wx4g1 wx4er wx4dz wx4g3 wx4fc]
 }
